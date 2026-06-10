45b8034e2bf5d048aacc51a150da220896c0526d
Edbert <edbertwu123@gmail.com>
2026-06-10 17:58:12 +0800

    feature: add GET /api/notes get all notes endpoint

--- services/notes-service/src/routes/notesRoutes.js
+++ services/notes-service/src/routes/notesRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import authenticate from '../middleware/authenticate.js'
-import { createNote } from '../controllers/notesController.js'
+import { createNote, getAllNotes } from '../controllers/notesController.js'
 
 const router = Router()
 
 router.post('/', authenticate, createNote)
+router.get('/', authenticate, getAllNotes)
 
 export default router
